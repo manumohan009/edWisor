07b37179edde2c7501b58cbf355143b2369d72bd
Manu <manumohan.cvav@gmail.com>
2018-10-29 00:01:10 +0530

    completed jquery events

---
+++ code/jquery5/index.js
@@ -0,0 +1,58 @@
+$(document).ready(() => {
+
+	// let myText = $('body').html();
+	// let myText = $('.heading').html();
+	// let myText = $('.paragraph').text();
+	// let myText = $('#someRandomId').text();
+	//let myText = $('.paragraph #newElement').text();
+	//alert(myText);
+
+	// let myImageSource = $(".my-image").attr("src");
+	// alert(myImageSource);
+
+	// let newParagraph = `This is a new paragraph to give you an example`;
+	// $('#newElement').text(newParagraph);
+
+	// $('.my-image').attr("src", "https://cnet3..... ");
+
+	// $(".paragraph").append("Some extra text inserted");
+	// $(".paragraph").prepend("Some extra text inserted");
+	// $(".paragraph").after("Some extra text inserted");
+	// $(".paragraph").before("Some extra text inserted");
+
+	// $("#someRandomId").remove();
+	//  $("#someRandomId").empty();
+//  --------------------------------------------------------
+
+
+	// $("#performSomeAction").click(() =>{
+	// 	$(".paragraph").addClass('big-font');
+	// 	//$(".paragraph").removeClass('big-font');
+	// })
+
+	// $('#newElement').dblclick(()=> {
+	// 	$('.paragraph').css('color','red');
+	// })
+
+	// $('#some-image').hover(() => {
+	// 	$('.my-image').hide('1000000');
+	// })
+//  --------------------------------------------------------
+
+// $('input').focus(function(){
+// 	$(this).addClass('colored-border')
+// });
+
+// $('input').focusout(function(){
+// 	$(this).removeClass('colored-border')
+// });
+
+// $('form').submit((e)=>{
+// 	e.preventDefault();
+// 	alert('user logged in succesfully')
+// });
+
+// $('#email-input').trigger('focus');
+
+	
+})// end doc ready
